b438ddf2897bbd9513ef53bb9e7f58c4743bcd26
Sridhar B <sridharkalaibala@gmail.com>
2015-12-15 21:57:32 +0530

    initial commit

---
+++ Source/application/views/editor.php
@@ -0,0 +1,4 @@
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/ckeditor/ckeditor.js"></script>
+<?php
+echo $this->ckeditor->editor("textarea name", "default textarea value");
+?>
